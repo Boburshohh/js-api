1eb3d0f5932a1a4e2ca3e2921a9f7326640e07c5
Boburshoh Khusanov <83607069+Boburshohh@users.noreply.github.com>
2022-07-25 16:12:00 +0500

    Update app.js

--- asist/app.js
+++ asist/app.js
@@ -18,3 +18,30 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
     // Apidagi xatolik uchun
     .catch(err => console.log('Something went wrong: ', err));
+
+
+
+
+
+
+
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //     let output = '';
+    //     console.log(data)
+    //     data.forEach(function (todo) {
+    //     output += `
+    //         <tr>
+    //             <td> ${todo.id}</td> 
+    //             <td> ${todo.name}</td>
+    //             <td> ${todo.email}</td>
+    //         </tr>
+    //         `;
+    //     });
+    //     document.getElementById('ToDoList').innerHTML = output;
+    //     return output;
+    // })
+
+    // // Apidagi xatolik uchun
+    // .catch(err => console.log('Something went wrong: ', err));
